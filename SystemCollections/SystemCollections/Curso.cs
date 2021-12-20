5aafbc06a4c65c0e45915025b6ef239a8868f365
Paulo Anderson Oliveira Castelo <paulo.castelo@actdigital.com>
2021-12-20 15:44:08 -0300

    Commit

---
+++ SystemCollections/SystemCollections/Curso.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemCollections
+{
+    class Curso
+    {
+        private IList<Aula> aulas;
+        private string nome;
+        private string instrutor;
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+            this.aulas = new List<Aula>();
+        }
+
+        internal void Adiciona(Aula aula)
+        {
+            this.aulas.Add(aula);
+        }
+
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula> (aulas); }
+            //set { aulas = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Instrutor
+        {
+            get { return instrutor; }
+            set { instrutor = value; }
+        }
+
+
+    }
+}
---
+++ SystemCollections/SystemCollections/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemCollections;
+
+namespace Aula24ListaSomentLeitura
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpcollection = new Curso("C# Collections P1", "Gustavo Instrutor");
+            csharpcollection.Adiciona(new Aula("Arrays", 45));
+            Imprimir(csharpcollection.Aulas);
+            Console.WriteLine();
+
+            //Adicionar 02 aulas
+            csharpcollection.Adiciona(new Aula("Criando uma aula.", 20));
+            csharpcollection.Adiciona(new Aula("Modelando Coleções", 19));
+
+            //Imprimir
+            Imprimir(csharpcollection.Aulas);
+
+            //Utilizar o Sort na lista
+            csharpcollection.Aulas.OrderBy();
+
+            Console.ReadLine();
+        }
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+}
